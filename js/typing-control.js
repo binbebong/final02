2cf80aaadbcf23df77bf4d35ae033dd682312109
binbebong <144688240+binbebong@users.noreply.github.com>
2023-10-02 18:20:53 +0700

    Add files via upload

---
+++ js/typing-control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+         strings: ["Graphic design.","Illustration.","Indesign."],
+    typeSpeed:20,
+        backSpeed:20,
+        loop:true,
+      });
